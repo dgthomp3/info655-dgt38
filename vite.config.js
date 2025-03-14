0cc555487035337334fe20cb6332ef9ad8f89269
Dallas Thompson III <dgthomp96@gmail.com>
2025-03-13 21:28:46 -0400

    set up for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/info655-dgt38/',
   test: {
     globals: true, // Enable global expect
     environment: "jsdom", // Simulates a browser-like environment
